2fca58828de397776f999b54f8498a58b1075404
GuiDevloper <guicorrea384@gmail.com>
2021-03-27 20:21:31 -0300

    Improving and modularizing code

--- index.js
+++ index.js
@@ -1,39 +1,19 @@
 #! /usr/bin/env node
 
-const { spawn } = require('child_process');
-const message = require('./messages');
+const {
+  showExit,
+  commands,
+  args,
+  run,
+  message
+} = require('./modules');
+const { add } = require('./add');
 
-const args = process.argv.slice(2);
-const commands = {
-  start: 'webpack --config node_modules/nullstack/webpack.config.js --mode=development --watch',
-  build: 'webpack --config node_modules/nullstack/webpack.config.js --mode=production'
-}
-const startCommand = /^win/.test(process.platform) ? 'npx.cmd' : 'npx';
-
-if (Object.keys(commands).includes(args[0])) {
-  console.log('npx ' + commands[args[0]]);
-} else {
-  console.log(message);
-  process.exit(0);
+if (!Object.keys(commands).includes(args[0])) {
+  showExit(message.unknown);
 }
 
-const d1 = new Date().getTime();
-const pawn = spawn(
-  startCommand,
-  commands[args[0]].split(' '),
-  { stdio: 'inherit' }
-);
-
-pawn.on('error', (err) => {
-  console.log(err);
-  process.exit(0);
-});
-
-pawn.on('close', () => {
-  if (args[0] === 'build') {
-    const d2 = new Date().getTime();
-    const duration = ((d2 - d1) / 1000).toFixed(2) + 's'
-    console.log('Done in', duration);
-  }
-  process.exit(0);
-});
+if (args[0] === 'add') {
+  add(args);
+}
+run();
---
+++ modules.js
@@ -0,0 +1,34 @@
+const message = require('./messages');
+const { spawn } = require('child_process');
+const args = process.argv.slice(2);
+const startCommand = /^win/.test(process.platform) ? 'npx.cmd' : 'npx';
+
+function showExit(message) {
+  console.log(message);
+  process.exit(0);
+}
+
+const webpackCommand = 'webpack --config node_modules/nullstack/webpack.config.js';
+const commands = {
+  start: webpackCommand + ' --mode=development --watch',
+  build: webpackCommand + ' --mode=production',
+  add: `${args.includes('-n') ? 'npm install' : 'yarn add'} ${args[1]} -D`
+};
+
+function run() {
+  const pawn = spawn(
+    startCommand,
+    commands[args[0]].split(' '),
+    { stdio: 'inherit' }
+  );
+  pawn.on('error', showExit);
+  pawn.on('close', () => process.exit(0));
+}
+
+module.exports = {
+  showExit,
+  commands,
+  args,
+  run,
+  message
+};
