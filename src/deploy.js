5d0eaecdf66dfc65225c869fd4da425afdb1976b
GuiDevloper <guicorrea384@gmail.com>
2021-05-24 14:25:08 -0300

    Reusing copyFile method

--- src/deploy.js
+++ src/deploy.js
@@ -14,17 +14,6 @@ function runCLI(args, cli, stdio = 'pipe') {
   return pawn;
 }
 
-function copyFile(myFile) {
-  const myPath = (file) => path.join(__dirname, `/deployFiles/vercel/${file}`);
-  const thisPath = (other) => path.join(process.cwd(), other || '');
-  if (!fs.existsSync(myFile)) {
-    const newDir = myFile.substring(0, myFile.lastIndexOf('/'));
-    fs.mkdirSync(path.join('./', newDir), { recursive: true });
-    fs.writeFileSync(myFile, '');
-  }
-  fs.copyFileSync(myPath(myFile), thisPath(myFile));
-}
-
 function rmDir(dir) {
   try {
     fs.rmdirSync(dir, { recursive: true });
@@ -46,7 +35,9 @@ function prepareVercelFiles() {
   try {
     fs.mkdirSync('./api');
   } catch {}
-  ['api/nullstack.js', 'vercel.json'].forEach(copyFile);
+  ['api/nullstack.js', 'vercel.json'].forEach(file => {
+    Nulla.copyFile('vercel', file);
+  });
 }
 
 Deploy.vercel = function(args) {
@@ -79,11 +70,6 @@ Deploy.vercel = function(args) {
 }
 
 Deploy.deploy = function(args) {
-  if (args[1] === '--yml') {
-    const ymlPath = '.github/workflows/main.yml';
-    copyFile(ymlPath);
-    Nulla.showExit(`${Nulla.message.ymlCreated} "${ymlPath}"`);
-  }
   if (args[1] === 'vercel') return Deploy.vercel(args);
   Nulla.showExit(Nulla.message.unknown);
 }
--- src/modules.js
+++ src/modules.js
@@ -1,4 +1,6 @@
 const child = require('child_process');
+const fs = require('fs');
+const path = require('path');
 const Nulla = {};
 Nulla.message = require('./messages');
 Nulla.args = Nulla.args || process.argv.slice(2);
@@ -29,6 +31,18 @@ Nulla.run = function() {
   );
   pawn.on('error', Nulla.showExit);
   pawn.on('close', () => process.exit(0));
+}
+
+Nulla.copyFile = function(myFile, host) {
+  const myPath = (file) => path.join(__dirname, `/deployFiles/${host}/${file}`);
+  const thisPath = (other) => path.join(process.cwd(), other || '');
+
+  if (!fs.existsSync(myFile)) {
+    const newDir = myFile.substring(0, myFile.lastIndexOf('/'));
+    fs.mkdirSync(path.join('./', newDir), { recursive: true });
+    fs.writeFileSync(myFile, '');
+  }
+  fs.copyFileSync(myPath(myFile), thisPath(myFile));
 }
 
 module.exports = Nulla;
